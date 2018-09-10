5527eea6be16125cf8e23db9614cab6f98693527
Ildar Bikmamatov <vistoyn@gmail.com>
2018-09-10 10:00:29 +0000

    change email

--- BayrellCommon/__init__.py
+++ BayrellCommon/__init__.py
@@ -7,7 +7,7 @@ __version__ = "0.0.2"
 __description__ = "Bayrell Common Library"
 __license__ = "Apache License Version 2.0"
 __author__ = "Ildar Bikmamatov"
-__email__ = "ildar@bayrell.org"
+__email__ = "support@bayrell.org"
 __copyright__ = "Copyright 2016-2018"
 __url__ = "https://github.com/bayrell/common_py3"
 
